1b5e6a3f106b06acec83959e370deef6c0b5924c
aowubulao <635416616@qq.com>
2020-07-18 10:49:44 +0800

    Modify test

--- src/test/java/Tess4jTest.java
+++ src/test/java/Tess4jTest.java
@@ -1,6 +1,3 @@
-import net.sourceforge.tess4j.ITesseract;
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
 
 import java.io.File;
 
@@ -13,34 +10,7 @@ import static com.neoniou.ak.ArkScript.cutImage;
 public class Tess4jTest {
 
     public void test1() {
-        // 识别图片的路径（修改为自己的图片路径）
-        String path = "C:\\ak-temp\\temp_cut.png";
-
-        // 语言库位置（修改为跟自己语言库文件夹的路径）
-        String languagePath = "E:\\program\\tessdata\\tessdata_best";
-
-        File file = new File(path);
-        ITesseract instance = new Tesseract();
-
-        //设置训练库的位置
-        instance.setDatapath(languagePath);
-
-        cutImage(1255,24, 170, 45);
-        //cutImage(1329,762, 50, 32);
-        //chi_sim ：简体中文， eng    根据需求选择语言库
-        instance.setLanguage("ita");
-        String result = null;
-        try {
-            long startTime = System.currentTimeMillis();
-            result =  instance.doOCR(file);
-            long endTime = System.currentTimeMillis();
-            System.out.println("Time is：" + (endTime - startTime) + " 毫秒");
-        } catch (TesseractException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("result: ");
-        System.out.println(result);
+  
     }
 
     public void test2() {
